0a8089da7e0af61bcb381df89bf92182cf26c5dd
Bach Le <bach@bullno1.com>
2017-11-25 03:53:28 +0800

    Remove unused files from /usr/lib

--- board/rpi0/post-build.sh
+++ board/rpi0/post-build.sh
@@ -1,9 +1,16 @@
 #!/bin/sh -e
 
 BOARD_DIR="$(dirname $0)"
-
 cp -r "${BOARD_DIR}"/boot-overlay/* "${BINARIES_DIR}/rpi-firmware"
 
+rm -rf "${TARGET_DIR}/usr/include"
+rm -rf "${TARGET_DIR}/usr/lib/*.a"
+rm -rf "${TARGET_DIR}/usr/lib/*.la"
+rm -rf "${TARGET_DIR}/usr/lib/*.so"
+rm -rf "${TARGET_DIR}/usr/lib/*.so.*"
+rm -rf "${TARGET_DIR}/usr/lib/pkgconfig"
+rm -rf "${TARGET_DIR}/usr/bin/getconf"
+rm -rf "${TARGET_DIR}/usr/bin/curve_keygen"
 rm -rf "${TARGET_DIR}/var/log/*"
 mkdir -p "${TARGET_DIR}/var/log"
 
